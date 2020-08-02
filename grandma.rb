47a068a35de62af6586fbba094df64e68e788d52
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-02 21:40:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(string)
-if string = "I LOVE YOU GRANDMA!"
+if string == "I LOVE YOU GRANDMA!"
 return "I LOVE YOU TOO PUMKIN!"
-elsif string = string.upcase
+elsif string == string.upcase
 return "NO, NOT SINCE 1938!"
 elsif string != string.upcase
 return "HUH?! SPEAK UP, SONNY!"
